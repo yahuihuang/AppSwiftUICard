86b0b3f865f7c83be93e6dd8fdef719ad3afb3ca
Huang Grace <huang.yahui.grace@gmail.com>
2022-03-27 13:42:56 +0800

    畫面基本配置

--- AppSwiftUICard/ContentView.swift
+++ AppSwiftUICard/ContentView.swift
@@ -8,9 +8,43 @@
 import SwiftUI
 
 struct ContentView: View {
+    var emojis = ["👾", "🎨"]
+    
+    @State var emojisCount = 20
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("\(emojisCount)")
+                .padding()
+            
+            Spacer()
+            HStack {
+                remove
+                Spacer()
+                add
+            }.font(.largeTitle)
+                .padding(.horizontal, 20)
+        }
+    }
+    
+    // Button
+    var remove: some View {
+        Button {
+            if emojisCount > 1 {
+                emojisCount -= 1
+            }
+        } label: {
+            Image(systemName: "minus.circle")
+        }
+    }
+    
+    var add: some View {
+        Button {
+            if emojisCount < 20 {
+                emojisCount += 1
+            }
+        } label: {
+            Image(systemName: "plus.circle")
+        }
     }
 }
 
