94d6ccc09214be7f94df970dc2487c663daaf2a3
Huang Grace <huang.yahui.grace@gmail.com>
2022-03-27 13:31:45 +0800

    Initial Commit

---
+++ AppSwiftUICard/AppSwiftUICardApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppSwiftUICardApp.swift
+//  AppSwiftUICard
+//
+//  Created by grace on 2022/3/27.
+//
+
+import SwiftUI
+
+@main
+struct AppSwiftUICardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
